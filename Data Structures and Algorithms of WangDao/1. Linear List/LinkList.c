46b86aeba204c1e8cc0fea5cc333c10c08352aa9
LiuBaiWan-LEGION <liubaiwan592@outlook.com>
2025-03-04 13:32:20 +0800

    Add Data Structures and Algorithms of WangDao

---
+++ Data Structures and Algorithms of WangDao/1. Linear List/LinkList.c
@@ -0,0 +1,9 @@
+/**
+ * @FileName    :LinkList.c
+ * @Date        :2025-03-04 13:27:10
+ * @Author      :LiuBaiWan (https://github.com/LiuBaiWan592)
+ * @Version     :V1.0.0
+ * @Brief       :
+ * @Description :
+ */
+
---
+++ Data Structures and Algorithms of WangDao/1. Linear List/SeqList.c
@@ -0,0 +1,9 @@
+/**
+ * @FileName    :SeqList.c
+ * @Date        :2025-03-04 13:27:05
+ * @Author      :LiuBaiWan (https://github.com/LiuBaiWan592)
+ * @Version     :V1.0.0
+ * @Brief       :
+ * @Description :
+ */
+
