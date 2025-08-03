463764c46387415dcb7b147a12683c97bc691c07
LiuBaiWan-Ubuntu <liubaiwan592@outlook.com>
2025-08-03 23:54:32 +0800

    Update Bracket Match

--- Data Structures and Algorithms of WangDao/Problems/Stack/01_Bracket_Match.c
+++ Data Structures and Algorithms of WangDao/Problems/Stack/01_Bracket_Match.c
@@ -23,34 +23,34 @@
 
 #include "common.h"
 
-bool Bracket_Match(char *str, int length){
+bool Bracket_Match(char *str, int length) {
     SeqStack S = SeqStack_Init(length);         // Initialize a stack.
-    for(int i = 0; i < length; i++){            // Traverse the string from left to right.
-        if(str[i] == '(' || str[i] == '[' || str[i] == '{'){    
+    for (int i = 0; i < length; i++) {            // Traverse the string from left to right.
+        if (str[i] == '(' || str[i] == '[' || str[i] == '{') {
             SeqStack_Push(S, str[i]);           // if the current character is an opening bracket, push it into the stack
-        }else{
-            if(SeqStack_IsEmpty(S)){
+        } else {
+            if (SeqStack_IsEmpty(S)) {
                 return false;                   // when the current character is a closing bracket, if the stack is empty, return false
             }
 
             char topElem = SeqStack_Pop(S);     // pop the top element, If the stack is not empty after traversing the string, return false.
-            if(str[i] == ')' && topElem != '(')
+            if (str[i] == ')' && topElem != '(')
                 return false;
-            if(str[i] == ']' && topElem != '[')
+            if (str[i] == ']' && topElem != '[')
                 return false;
-            if(str[i] == '}' && topElem != '{')
+            if (str[i] == '}' && topElem != '{')
                 return false;
         }
     }
     return SeqStack_IsEmpty(S);                 // if the stack is empty, return true
 }
 
-int main(){
-    char input[100];
-    scanf("%s", input);
-    int length = strlen(input);
-    bool flag = Bracket_Match(input, length);
-    printf("flag = %s\n", flag ? "true" : "false");
+int main() {
+    char input[5][20] = {"{[()()]()}", "{[()([))]()}", "{[()()]()", "({[()()]()})[", "({[()()]()})[]"};
+    for (int i = 0; i < 5; i++) {
+        bool flag = Bracket_Match(input[i], strlen(input[i]));
+        printf("flag = %s\n", flag ? "true" : "false");
+    }
     return 0;
 }
 
