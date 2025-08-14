0a64893da15679ebb495bb7b0824cb14bb9064a2
LiuBaiWan-Ubuntu <liubaiwan592@outlook.com>
2025-08-14 21:55:07 +0800

    Update the Heap Sequence String, modify the SString to HString

--- Data Structures and Algorithms of WangDao/3.String/HString.c
+++ Data Structures and Algorithms of WangDao/3.String/HString.c
@@ -10,21 +10,21 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-typedef struct SString {
+typedef struct HString {
     char *ch;
     int length;
     int capacity;
-} *SString;
+} *HString;
 
-SString SString_Init(int capacity) {
-    SString S = (SString)malloc(sizeof(struct SString));
+HString HString_Init(int capacity) {
+    HString S = (HString)malloc(sizeof(struct HString));
     S->ch = (char *)malloc(sizeof(char) * (capacity + 1));
     S->length = 0;
     S->capacity = capacity;
     return S;
 }
 
-bool SString_Assign(SString S, char *str) {
+bool HString_Assign(HString S, char *str) {
     int i = 0;
     while (str[i] != '\0' && i < S->capacity) {
         S->ch[i + 1] = str[i];
@@ -38,15 +38,15 @@ bool SString_Assign(SString S, char *str) {
     return true;
 }
 
-bool SString_IsEmpty(SString S) {
+bool HString_IsEmpty(HString S) {
     return S->length == 0;
 }
 
-int SString_GetLength(SString S) {
+int HString_GetLength(HString S) {
     return S->length;
 }
 
-bool SString_Copy(SString S, SString T) {
+bool HString_Copy(HString S, HString T) {
     for (int i = 1; i <= S->length; i++) {
         T->ch[i] = S->ch[i];
     }
@@ -54,8 +54,8 @@ bool SString_Copy(SString S, SString T) {
     return true;
 }
 
-SString SString_Concat(SString S, SString T) {
-    SString ST = SString_Init(S->length + T->length);
+HString HString_Concat(HString S, HString T) {
+    HString ST = HString_Init(S->length + T->length);
     for (int i = 1; i <= S->length; i++) {
         ST->ch[i] = S->ch[i];
     }
@@ -66,8 +66,8 @@ SString SString_Concat(SString S, SString T) {
     return ST;
 }
 
-SString SString_SubString(SString S, int pos, int len) {
-    SString Sub = SString_Init(len);
+HString HString_SubString(HString S, int pos, int len) {
+    HString Sub = HString_Init(len);
     if (pos < 1 || pos > S->length || len < 0 || len > S->length - pos + 1) {
         printf("Warning: Invalid position or length!\n");
         return Sub;
@@ -79,7 +79,7 @@ SString SString_SubString(SString S, int pos, int len) {
     return Sub;
 }
 
-int SString_Compare(SString S, SString T) {
+int HString_Compare(HString S, HString T) {
     for (int i = 1; i <= S->length && i <= T->length; i++) {
         if (S->ch[i] != T->ch[i]) {
             return S->ch[i] - T->ch[i];
@@ -88,12 +88,12 @@ int SString_Compare(SString S, SString T) {
     return S->length - T->length;
 }
 
-int SString_Index(SString S, SString T) {
+int HString_Index(HString S, HString T) {
     int i = 1, n = S->length, m = T->length;
-    SString Sub = SString_Init(m);
+    HString Sub = HString_Init(m);
     while (i <= n - m + 1) {
-        Sub = SString_SubString(S, i, m);
-        if (SString_Compare(Sub, T) != 0) {
+        Sub = HString_SubString(S, i, m);
+        if (HString_Compare(Sub, T) != 0) {
             i++;
         } else {
             return i;
@@ -102,20 +102,20 @@ int SString_Index(SString S, SString T) {
     return 0;
 }
 
-bool SString_Clear(SString S) {
+bool HString_Clear(HString S) {
     S->length = 0;
     return true;
 }
 
-SString SString_Destroy(SString S) {
+HString HString_Destroy(HString S) {
     free(S->ch);
     free(S);    
     return NULL;
 }
 
-void SString_Print(SString S) {
-    printf("SString: ");
-    if (SString_IsEmpty(S)) {
+void HString_Print(HString S) {
+    printf("HString: ");
+    if (HString_IsEmpty(S)) {
         printf("Empty String!\n");
         return;
     } else {
@@ -134,45 +134,45 @@ void SString_Print(SString S) {
 }
 
 int main() {
-    SString S = SString_Init(10);
-    SString T = SString_Init(10);
-    SString_Assign(S, "Hello");
-    SString_Print(S);
+    HString S = HString_Init(10);
+    HString T = HString_Init(10);
+    HString_Assign(S, "Hello");
+    HString_Print(S);
 
-    printf("If S is empty: %s\n", SString_IsEmpty(S) ? "empty" : "not empty");
-    printf("Length of S: %d\n", SString_GetLength(S));
+    printf("If S is empty: %s\n", HString_IsEmpty(S) ? "empty" : "not empty");
+    printf("Length of S: %d\n", HString_GetLength(S));
 
-    SString_Copy(S, T);
-    SString_Print(T);
+    HString_Copy(S, T);
+    HString_Print(T);
 
-    SString_Assign(T, "World");
-    SString_Print(T);
+    HString_Assign(T, "World");
+    HString_Print(T);
 
-    SString ST = SString_Concat(S, T);
-    SString_Print(S);
-    SString_Print(T);
-    SString_Print(ST);
+    HString ST = HString_Concat(S, T);
+    HString_Print(S);
+    HString_Print(T);
+    HString_Print(ST);
 
-    SString Sub = SString_SubString(S, 3, 3);
-    SString_Print(Sub);
+    HString Sub = HString_SubString(S, 3, 3);
+    HString_Print(Sub);
 
-    printf("Compare S and ST: %d\n", SString_Compare(S, ST));
+    printf("Compare S and ST: %d\n", HString_Compare(S, ST));
 
-    printf("Index of T in ST: %d\n", SString_Index(ST, T));
+    printf("Index of T in ST: %d\n", HString_Index(ST, T));
 
-    SString_Clear(S);
-    SString_Print(S);
-    printf("If S is empty: %s\n", SString_IsEmpty(S) ? "empty" : "not empty");
+    HString_Clear(S);
+    HString_Print(S);
+    printf("If S is empty: %s\n", HString_IsEmpty(S) ? "empty" : "not empty");
 
-    S = SString_Destroy(S);
+    S = HString_Destroy(S);
     if (S == NULL) {
         printf("S is destroyed!\n");
 
     } else {
         printf("S is not destroyed!\n");
     }
-    T = SString_Destroy(T);
-    ST = SString_Destroy(ST);
-    Sub = SString_Destroy(Sub);
+    T = HString_Destroy(T);
+    ST = HString_Destroy(ST);
+    Sub = HString_Destroy(Sub);
     return 0;
 }
