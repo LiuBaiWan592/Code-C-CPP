86f04806563cf7ca929ed1b0ba5095c25b951a21
LiuBaiWan-LEGION <liubaiwan592@outlook.com>
2025-09-04 22:42:15 +0800

    Add the Sequential storage Binary Tree data structure file

---
+++ Data Structures and Algorithms of WangDao/4.Tree/SeqBinaryTree.c
@@ -0,0 +1,8 @@
+/**
+ * @FileName    :SeqBinaryTree.c
+ * @Date        :2025-09-08 13:47:35
+ * @Author      :LiuBaiWan (https://github.com/LiuBaiWan592)
+ * @Version     :V1.0.0
+ * @Brief       :Sequential binary tree with dynamic memory allocation
+ * @Description :
+ */
