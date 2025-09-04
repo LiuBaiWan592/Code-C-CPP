b3116cda6bdd01715f47ff567f2281519bd36c17
LiuBaiWan-LEGION <liubaiwan592@outlook.com>
2025-09-04 22:43:03 +0800

    Add the Linked Node Binary Tree data structure  file

---
+++ Data Structures and Algorithms of WangDao/4.Tree/LinkBinaryTree.c
@@ -0,0 +1,9 @@
+/**
+ * @FileName    :LinkBinaryTree.c
+ * @Date        :2025-09-10 22:40:36
+ * @Author      :LiuBaiWan (https://github.com/LiuBaiWan592)
+ * @Version     :V1.0.0
+ * @Brief       :
+ * @Description :
+ */
+
