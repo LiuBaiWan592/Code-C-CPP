d8aea72dedf12b465e7645b2f0829027b229be02
LiuBaiWan-LEGION <liubaiwan592@outlook.com>
2025-09-05 22:44:43 +0800

    Update the SeqBinaryTree, add the SeqBinaryTree node and the tree structure

--- Data Structures and Algorithms of WangDao/4.Tree/SeqBinaryTree.c
+++ Data Structures and Algorithms of WangDao/4.Tree/SeqBinaryTree.c
@@ -6,3 +6,20 @@
  * @Brief       :Sequential binary tree with dynamic memory allocation
  * @Description :
  */
+
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ElemType int
+
+typedef struct TreeNode {
+    ElemType data;
+} TreeNode;
+
+typedef struct SeqBinaryTree {
+    TreeNode *data;
+    int length;
+    int capacity;
+} *SeqBinaryTree;
