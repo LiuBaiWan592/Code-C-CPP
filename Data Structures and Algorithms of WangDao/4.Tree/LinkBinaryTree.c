30711f270d403768177c11d4e846063abe7b12e6
LiuBaiWan-LEGION <liubaiwan592@outlook.com>
2025-09-09 00:00:55 +0800

    Update the LinkBinaryTree,

--- Data Structures and Algorithms of WangDao/4.Tree/LinkBinaryTree.c
+++ Data Structures and Algorithms of WangDao/4.Tree/LinkBinaryTree.c
@@ -3,7 +3,12 @@
  * @Date        :2025-09-10 22:40:36
  * @Author      :LiuBaiWan (https://github.com/LiuBaiWan592)
  * @Version     :V1.0.0
- * @Brief       :
+ * @Brief       :Binary tree with linked storage
  * @Description :
  */
 
+#include <stdio.h>
+
+int main() {
+    return 0;
+}
